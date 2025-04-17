0dd3dac816514b2a39dc2225664dd18d4b22ab3c
suix0 <112095627+suix0@users.noreply.github.com>
2025-04-17 15:46:29 +0800

    feat: added validation on user registration

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,38 +1,61 @@
 const { PrismaClient } = require("../generated/prisma");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 
 const prisma = new PrismaClient();
 
-exports.registerUser = async (req, res) => {
-  // Check if username already exists
-  const user = await prisma.user.findMany({
-    where: {
-      username: req.body.username,
-    },
-  });
+const validateUser = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username can't be empty.")
+    .custom(async (value) => {
+      const username = await prisma.user.findMany({
+        where: {
+          username: value,
+        },
+      });
+      if (username.length > 0) {
+        throw new Error("User already exists.");
+      }
+    }),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password can't be empty.")
+    .isLength({ min: 8 })
+    .withMessage("Password must have a minimum of 8 characters"),
+];
 
-  if (user.length > 0) {
-    return res.json({
-      error: "Username already exists",
+exports.registerUser = [
+  validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Otherwise, register the user
+    await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: encryptedPassword,
+        role: req.body.role === "ADMIN" ? "ADMIN" : "USER",
+      },
     });
-  }
 
-  const encryptedPassword = await bcrypt.hash(req.body.password, 10);
-
-  // Otherwise, register the user
-  await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: encryptedPassword,
-      role: req.body.role === "ADMIN" ? "ADMIN" : "USER",
-    },
-  });
-
-  res.json({
-    message: "User created successfully",
-  });
-};
+    res.json({
+      message: "User created successfully",
+    });
+  },
+];
 
 exports.loginUser = async (req, res) => {
   // Check if user exist
