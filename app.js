c4d0cf896561fc83d2e8f2ba526b3c8733fb60c9
suix0 <112095627+suix0@users.noreply.github.com>
2025-05-04 19:15:23 +0800

    added host

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use("/api/authors", authorsRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
