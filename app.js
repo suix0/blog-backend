717f890eaad3cdbff2cff44edc45ef26216ee402
suix0 <112095627+suix0@users.noreply.github.com>
2025-05-04 19:36:10 +0800

    fix: updated cors origin

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/api/users", usersRouter);
 app.use("/api/posts", postsRouter);
